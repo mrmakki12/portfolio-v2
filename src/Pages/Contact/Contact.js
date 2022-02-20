89c3531991b1fd7752d0a94c274d32e798d8e35b
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-02-20 11:22:32 -0500

    fixed security issue + aria-labels

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -12,8 +12,8 @@ export const Contact = () => {
                     It can be about anything really.
                 </p>
                 {/* links to linkedIn and Github */}
-                <a href='https://www.linkedin.com/in/tyreeck-makki/' target='_blank'><span aria-hidden="true" className="devicons devicon-linkedin-plain colored" title="My LinkedIn"></span></a>
-                <a href='https://github.com/mrmakki12' target='_blank'><span aria-hidden="true" className="devicons devicon-github-original" title="My Github"></span></a>
+                <a href='https://www.linkedin.com/in/tyreeck-makki/' aria-label="Tyreeck's LinkedIn" target='_blank' rel='noreferrer'><span aria-hidden="true" className="devicons devicon-linkedin-plain colored" title="My LinkedIn"></span></a>
+                <a href='https://github.com/mrmakki12' aria-label="Tyreeck's Github" target='_blank' rel='noreferrer'><span aria-hidden="true" className="devicons devicon-github-original" title="My Github"></span></a>
             </div>
             {/* contact form, fades left on scroll*/}
             <div className='contact' data-aos='fade-left'>
